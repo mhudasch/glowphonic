e97e67243584a2fce631c078b20cebcb3c2b871e
Martin Hudasch <martin.hudasch@live.de>
2025-03-22 15:44:50 +0000

    changes

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,5 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello from Zig dev-pod!\n", .{});
+    try stdout.print("Hello from Zig dev-pod-changed!\n", .{});
 }
